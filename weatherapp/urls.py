7ff136b28ed26e125f53b3fb2a1594148f62e5ea
Kk <iankk0806@gmail.com>
2024-10-12 01:55:39 +0800

    get response back

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_data),
+    path('', views.get_data,name="get_data"),
 ]
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 import requests
+import json
+
 
 # Create your views here.
 def get_data():
-  url="https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001"
+  url="https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
   params={
     "Authorization":"CWA-84EE207C-D4D1-46E7-A809-0970F6728AD9",
+    "format": "JSON",
     "locationName":"臺北市"
   }
-  response = requests.get(url, params=params)
+  headers = {
+        "accept": "application/json"
+    }
+  response = requests.get(url, params=params,headers=headers)
   print(response.status_code)
 
   if response.status_code == 200:
@@ -26,14 +32,15 @@ def get_data():
         comfort = weather_elements[3]["time"][0]["parameter"]["parameterName"]
         max_tem = weather_elements[4]["time"][0]["parameter"]["parameterName"]
 
-        print(location)
-        print(start_time)
-        print(end_time)
-        print(weather_state)
-        print(rain_prob)
-        print(min_tem)
-        print(comfort)
-        print(max_tem)
+        print("地點："+location)
+        print("預測開始時間："+start_time)
+        print("預測結束時間："+end_time)
+        print("天氣型態："+weather_state)
+        print("降雨機率："+rain_prob)
+        print("最低氣溫："+min_tem)
+        print("氣溫狀態："+comfort)
+        print("最高氣溫："+max_tem)
 
   else:
         print("Can't get data!")
+
